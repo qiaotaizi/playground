818c06bfce1f38f1ff3a41db38224e62e653cf27
zhiheng.jiang <gracia2015@126.com>
2020-01-08 20:23:28 -0800

    开始测试Objects api

---
+++ src/test/java/com/jaiz/study/ObjectsTest.java
@@ -0,0 +1,14 @@
+package com.jaiz.study;
+
+import org.junit.Test;
+
+import java.util.Objects;
+
+public class ObjectsTest {
+
+
+    @Test
+    public void xxTest(){
+        Objects.equals()
+    }
+}
--- src/test/java/com/jaiz/study/SteamTest.java
+++ src/test/java/com/jaiz/study/SteamTest.java
@@ -8,9 +8,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.function.Supplier;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
